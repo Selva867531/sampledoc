82d703f780924b5aef501fcd299d7c8da2e3b2db
selva <selvabalasubramanian25@gmail.com>
2024-07-19 11:09:13 +0530

    nextjs dash commit

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,9 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit pariatur explicabo consectetur molestias necessitatibus quasi minima totam eum ducimus harum labore quam in incidunt suscipit, excepturi nesciunt, reprehenderit voluptatum soluta officiis? Eos quo quaerat ad laudantium dolorem magnam saepe corrupti recusandae ullam perferendis, dolore molestiae officiis non sunt velit a animi sint delectus fuga ea similique aspernatur fugiat! Quae, iusto! Quas molestiae voluptatum inventore, harum ducimus sunt voluptatem perferendis odio ratione quibusdam? Placeat repellat ratione nobis sequi quo corrupti ipsam facere unde ipsum deserunt laudantium provident non amet temporibus accusantium, dolorem nihil repellendus obcaecati modi dolorum? Quia minima nihil pariatur.
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aperiam nihil vero, quas rerum quos possimus dolorem vitae recusandae sequi fugiat voluptas blanditiis! Molestiae, dolore incidunt! Officia veritatis porro adipisci quia ad praesentium nemo qui iure nisi magni nostrum, itaque quis quidem totam officiis beatae? Earum ipsa velit laboriosam nostrum iure explicabo aspernatur, laborum asperiores, debitis illum fugiat doloribus ut aliquid porro delectus adipisci sed, voluptas fuga dolorum. Accusamus distinctio modi asperiores eveniet autem expedita, fugit nihil, praesentium quo laboriosam porro consequuntur quod voluptas cupiditate non ipsa labore tempora! Repellat veritatis consequatur corporis enim nesciunt. Aperiam corporis nam officiis iste.</p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
